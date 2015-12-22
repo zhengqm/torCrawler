3b2ecb8ddb116cfd53f11e75a675b8efc7cdb2f2
Qimu Zheng <zheng.qm@163.com>
2015-12-22 21:28:46 +0800

    More about tor

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import threading
 import time
 import random
-from stem.control import Controller, Signal
-#import requests
+import requesocks
 
+from stem.control import Controller, Signal
 from Queue import Queue
 
-def load_template(config_file):
-    lines = filter(lambda x: len(x) > 0 and x[0] != '#', open(config_file).read().split('\n'))
+def load_template(template_file):
+    lines = filter(lambda x: len(x) > 0 and x[0] != '#', open(template_file).read().split('\n'))
     return map(lambda x: x.split(','), lines)
 
-def download_data(queue, template, to_sleep, config, build_request):
+def download_data(queue, templates, to_sleep, config):
+    session = requesocks.session()
+
+    session.proxies = {
+        'http': '{}://{}:{}'.format(config['protocal'], config['address'], config['port']),
+        'https': '{}://{}:{}'.format(config['protocal'], config['address'], config['port']),
+    }
+
     while True:
         try:
             n = queue.get(timeout=0.1)
         except Exception:
             return
 
+        # Download the specified page
         print "Downloading data of bug_{}".format(n)
-        
+
+        for url_template, file_template in templates:
+            url = url_template.format(n)
+            resp = session.get(url)
+            with open(file_template.format(n), 'w') as f:
+                f.write(resp.text)
+
+        # Finish downloading...
+
         if queue.empty():
             return
         
@@ -27,10 +43,19 @@ def download_data(queue, template, to_sleep, config, build_request):
         
 
 def change_circuit(controller, to_sleep):
-    while True:
-        time.sleep(random.randint(1, to_sleep))
+    while True:        
         controller.signal(Signal.NEWNYM)
+        time.sleep(to_sleep)
         print "Changing circuit"
+        circuits = controller.get_circuits()
+
+        print "End Point:"
+        for circ in circuits:
+            entry = circ.path[-1]
+            fingerprint, nickname = entry
+            desc = controller.get_network_status(fingerprint, None)
+            address = desc.address if desc else 'unknown'
+            print address
         
 
 
@@ -39,33 +64,37 @@ def config_tor_browser():
     controller.authenticate()
     return controller
 
-def build_request(url, config):
-    pass
-    # return request
+
+def load_config(config_file):
+    lines = filter(lambda x: len(x) > 0 and x[0] != '#', open(config_file).read().split('\n'))
+    return dict(map(lambda x: x.split(':'), lines))
 
 if __name__ == '__main__':
     controller = config_tor_browser()
     template = load_template('template.txt')
     download_threads = 10
     config = load_config('configuration.txt')
-
     queue = Queue()
 
+    # Initialize queue
     start = 1
     end = 100
     for i in range(start, end+1):
         queue.put(i)
 
 
-    t1 = threading.Thread(target = change_circuit, args = (None, 3,))
+    # Initialize thread for changing circuit
+    t1 = threading.Thread(target = change_circuit, args = (None, 60,))
     t1.daemon = True
     t1.start()
 
+    # Initialize thread for downloading pages
     for _ in range(download_threads):
-        t = threading.Thread(target=download_data, args = (queue, template, 5, config, build_request,))
+        t = threading.Thread(target=download_data, args = (queue, template, 10, config,))
         t.daemon = True
         t.start()
     
+    # Wait for downloading
     while threading.active_count() > 2:
         time.sleep(0.1)
 
